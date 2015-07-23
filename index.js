31ec86e9d4bded53dd713319214a5a30dc2a35bd
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-07-23 11:32:48 -0700

    formatting

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ var express = require('express'),
     fs = require('fs'),
     path = require('path');
 var spawn = require('child_process').spawn;
+var RoundRobin = require('./RoundRobin');
 
 var gm = require('gm')
 var app = express();
 
+var manager = new RoundRobin(1);
 // All your express server code goes here.
 // ...
 app.use(express.static(__dirname + '/public'));
@@ -17,23 +19,30 @@ app.post('/upload', function(req, res) {
   var form = new formidable.IncomingForm();
   form.parse(req, function(err, fields, files) {
     // `file` is the name of the <input> field of type `file`
-    var old_path = files.file.path,
-    file_size = files.file.size,
-    file_ext = files.file.name.split('.').pop(),
-    index = old_path.lastIndexOf('/') + 1,
-    file_name = old_path.substr(index),
-    new_path = path.join(process.env.PWD, '/uploads/', file_name + '.' + file_ext);
+    var old_path = files.file.path;
+    var file_size = files.file.size;
+    var file_ext = files.file.name.split('.').pop();
+    var index = old_path.lastIndexOf('/') + 1;
+    var file_name = old_path.substr(index);
+    var new_path = path.join(process.env.PWD, '/uploads/', file_name + '.' + file_ext);
     fs.readFile(old_path, function(err, data) {
       fs.writeFile(new_path, data, function(err) {
         fs.unlink(old_path, function(err) {
-          spawn('./worker.js', new_path);
-          if (err) {
-            res.status(500);
-            res.json({'success': false});
-          } else {
-            res.status(200);
-            res.json({'success': true});
-          }
+          var data = {
+            new_path: new_path
+          };
+          manager.giveWork(data, function(err, outputpath){
+            if (err) {
+              res.status(500);
+              res.json({'success': false});
+            } else {
+              res.status(200);
+              res.json({'success': true});
+            }
+
+
+          }); 
+
         });
       });
     });
--- processor.js
+++ processor.js
@@ -1,24 +1,26 @@
 var gm = require('gm')
 var path = require('path');
-module.exports = function(filepath, next) {
-  var startTime = new Date();
+module.exports = function(obj, next) {
+  var filepath = obj.new_path;
   var outputPath = __dirname + '/charcoal/' + path.basename(filepath);
-  gm(file_path)
+  gm(filepath)
     .flip()
     .magnify()
     .rotate('green', 45)
     .blur(7, 3)
     .edge(3)
     .write(outputPath, function (err) {
+      console.log(err);
       if(err) return next(err);
       if (!err) console.log('crazytown has arrived');
+      // call next with no error
       next(void(0), outputPath);
     });
 };
 
 if(!module.parent) {
   process.on("message", function(data) {
-    module.exports( data.filepath, function(err, outputpath) {
+    module.exports(data.filepath, function(err, outputpath) {
       process.send({
         id: data.id,
         err: err,
